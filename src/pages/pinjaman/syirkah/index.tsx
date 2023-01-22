36744cde1237ca484c1270cb118e62701673960c
aira1001 <adindakhaerani95@gmail.com>
2023-01-22 23:30:07 +0700

    create table, menu syirkah

---
+++ src/pages/pinjaman/syirkah/index.tsx
@@ -0,0 +1,135 @@
+import { Box, Button, Card, CardBody, CardHeader, Divider, Flex, Heading, Icon, Input, InputGroup, InputLeftElement, Spacer, Table, TableContainer, Tbody, Text, Th, Thead, Tr } from "@chakra-ui/react"
+import { MagnifyingGlassIcon, PlusIcon } from "@heroicons/react/24/outline"
+import { GetServerSideProps } from "next"
+
+import BreadcrumbSection from "@/components/BreadcrumbSection"
+import TableSyirkah from "@/components/Tables/TableSyirkah"
+
+type TPageProps = {
+  pageTitle: string
+}
+
+export const getServerSideProps: GetServerSideProps<TPageProps> = async () => {
+  return {
+    props: {
+      pageTitle: 'Syirkah'
+    }
+  }
+}
+
+const dataSyirkah = [{
+  namaBC: 'makan malam',
+  namaAnggota: 'admin',
+  idAnggota: '123456789',
+  modalAwal: 'cicil rumah',
+  modalHamasah: 'Rp.10.900.000',
+  tglMulai: 'Rp.27.800.000',
+  tglSelesai: '30 September 2022',
+}, {
+  namaBC: 'makan malam',
+  namaAnggota: 'admin',
+  idAnggota: '123456789',
+  modalAwal: 'cicil rumah',
+  modalHamasah: 'Rp.10.900.000',
+  tglMulai: 'Rp.27.800.000',
+  tglSelesai: '30 September 2022',
+}, {
+  namaBC: 'makan malam',
+  namaAnggota: 'admin',
+  idAnggota: '123456789',
+  modalAwal: 'cicil rumah',
+  modalHamasah: 'Rp.10.900.000',
+  tglMulai: 'Rp.27.800.000',
+  tglSelesai: '30 September 2022',
+}, {
+  namaBC: 'makan malam',
+  namaAnggota: 'admin',
+  idAnggota: '123456789',
+  modalAwal: 'cicil rumah',
+  modalHamasah: 'Rp.10.900.000',
+  tglMulai: 'Rp.27.800.000',
+  tglSelesai: '30 September 2022',
+}, {
+  namaBC: 'makan malam',
+  namaAnggota: 'admin',
+  idAnggota: '123456789',
+  modalAwal: 'cicil rumah',
+  modalHamasah: 'Rp.10.900.000',
+  tglMulai: 'Rp.27.800.000',
+  tglSelesai: '30 September 2022',
+}, {
+  namaBC: 'makan malam',
+  namaAnggota: 'admin',
+  idAnggota: '123456789',
+  modalAwal: 'cicil rumah',
+  modalHamasah: 'Rp.10.900.000',
+  tglMulai: 'Rp.27.800.000',
+  tglSelesai: '30 September 2022',
+}]
+
+export default function PageSyirkah() {
+  const breadcrumbData = [
+    {
+      name: 'Pinjaman',
+    },
+    {
+      name: 'Syirkah',
+    },
+  ];
+  return (
+    <>
+      <Box>
+        <Box>
+          <BreadcrumbSection data={breadcrumbData} />
+        </Box>
+        <Card>
+          <CardHeader>
+            <Flex mb={3}>
+              <Box>
+                <Heading size='md'>Data Syirkah Anggota</Heading>
+              </Box>
+              <Spacer />
+              <Box>
+                <Button colorScheme='teal'><Icon as={PlusIcon} />&nbsp;Tambah Pinjaman</Button>
+              </Box>
+            </Flex>
+            <Divider />
+            <Box w="300px" mt={3}>
+              <Text fontSize='sm'>Pencarian</Text>
+              <InputGroup mt={2}>
+                <InputLeftElement
+                  pointerEvents='none'>
+                  <Icon as={MagnifyingGlassIcon} color='gray' />
+                </InputLeftElement>
+                <Input placeholder="cari berdasarkan nama" focusBorderColor="teal.200" />
+              </InputGroup>
+            </Box>
+          </CardHeader>
+          <CardBody mt={-5}>
+            <TableContainer p={3} boxShadow='md'>
+              <Table size='sm'>
+                <Thead>
+                  <Tr>
+                    <Th>Nama BC</Th>
+                    <Th>Nama Anggota</Th>
+                    <Th>ID Anggota</Th>
+                    <Th>Modal Awal</Th>
+                    <Th>Modal Hamasah</Th>
+                    <Th>Tanggal Mulai</Th>
+                    <Th>Tanggal Seleksi</Th>
+                    <Th>Aksi</Th>
+                  </Tr>
+                </Thead>
+                <Tbody>
+                  {dataSyirkah.map((item, index) => (
+                    <TableSyirkah key={index} item={item} />
+                  ))}
+                </Tbody>
+              </Table>
+            </TableContainer>
+          </CardBody>
+        </Card>
+      </Box>
+    </>
+  )
+}
