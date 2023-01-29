03721cef55310d0233fd09df047558e653c05514
hafizabd <abduromanov11@gmail.com>
2023-01-29 23:27:10 +0700

    fix: simpanan api

--- src/pages/api/simpanan/index.ts
+++ src/pages/api/simpanan/index.ts
@@ -1,18 +1,24 @@
+import _ from "lodash";
 import { NextApiRequest, NextApiResponse } from "next";
 
 import directus from "@/services/api/directus";
 
-import { TResponse, TSimpanan } from "@/types";
+import { DirectusResponse, TAnggota, TResponse } from "@/types";
 
 export default async function handler(
   req: NextApiRequest,
-  res: NextApiResponse<TResponse | TSimpanan>
+  res: NextApiResponse<TResponse | DirectusResponse<TAnggota[]>>
 ) {
   try {
-    const data = await directus.items('mutasiTabungan').readByQuery({
-      fields: ['*'],
-      meta: '*',
-      ...req.query,
+    const data = await directus.items('anggota').readByQuery({
+      fields: ['id', 'idAnggota', 'nama', 'alamat', 'mutasiTabungan.saldo'],
+    });
+
+    data.data?.map(item => {
+      item.totalSimpanan = _.reduce(item.mutasiTabungan, (total, mutasi) => (total?.saldo || 0) + (mutasi?.saldo || 0)) || 0
+      delete item.mutasiTabungan
+
+      return item;
     });
 
     return res.status(200).json(data);
