faca1d6c9a693e87830de8fbe397270646d9876f
hafizabd <abduromanov11@gmail.com>
2023-02-23 23:53:28 +0700

    fix 404 page

--- src/pages/404.tsx
+++ src/pages/404.tsx
@@ -1,4 +1,8 @@
-import { Heading, Stack } from "@chakra-ui/react";
+import { Button, Heading, Stack } from "@chakra-ui/react";
+import Head from "next/head";
+import Link from "next/link";
+
+import { NextPageWithLayout } from "./_app";
 
 export default function Custom404() {
   return (
@@ -8,8 +12,20 @@ export default function Custom404() {
       justifyContent={"center"}
       spacing={"6"}
     >
+      <Head>
+        <title>404 - {process.env.APP_NAME}</title>
+      </Head>
+
       <Heading size={"4xl"}>404</Heading>
-      <p>Page Not Found</p>
+      <p>Halaman tidak ditemukan</p>
+
+      <Link href={'/'}>
+        <Button colorScheme='brand'>Kembali ke Home</Button>
+      </Link>
     </Stack>
   );
 }
+
+Custom404.getLayout = function getLayout(page: NextPageWithLayout) {
+  return page;
+};
