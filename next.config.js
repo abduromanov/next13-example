c3134d9af8681f1b9f5ae1511bb32ea423a63be8
abduromanov <abduromanov@users.noreply.github.com>
2023-02-27 05:25:42 +0000

    Prettified Code!

--- next.config.js
+++ next.config.js
@@ -3,13 +3,13 @@ const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
   images: {
-    domains: ['directus.imj.sch.id']
+    domains: ["directus.imj.sch.id"],
   },
   env: {
     APP_NAME: process.env.NEXT_APP_NAME,
     API_URL: process.env.NEXT_API_URL,
-    API_TOKEN: process.env.NEXT_API_TOKEN
+    API_TOKEN: process.env.NEXT_API_TOKEN,
   },
-}
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
