491ec2cedff13d4ba99dd054f9612d387be3802c
abduromanov <abduromanov@users.noreply.github.com>
2023-03-25 07:29:08 +0000

    Prettified Code!

--- src/services/utils/font.ts
+++ src/services/utils/font.ts
@@ -1,9 +1,9 @@
 import { Inter } from "@next/font/google";
 
 const font = Inter({
-  subsets: ['latin'],
-  variable: '--font-poppins',
-  weight: ['100', '200', '300', '400', '500', '600', '700']
-})
+  subsets: ["latin"],
+  variable: "--font-poppins",
+  weight: ["100", "200", "300", "400", "500", "600", "700"],
+});
 
 export default font;
