9bf7c769f4129ee211f45821e1f983ae33015e21
hafizabd <abduromanov11@gmail.com>
2023-04-01 23:34:42 +0700

    fix: index page

--- src/pages/anggota/index.tsx
+++ src/pages/anggota/index.tsx
@@ -1,10 +1,5 @@
 import {
   Box,
-  Card,
-  CardBody,
-  CardHeader,
-  Divider,
-  Heading,
   Skeleton,
   Stack,
 } from "@chakra-ui/react";
@@ -53,45 +48,35 @@ export default function Page() {
 
   return (
     <Stack spacing="8">
-      <Box>
-        <Card w={"full"}>
-          <CardHeader>
-            <Heading size={"md"}>Pengumuman</Heading>
-          </CardHeader>
-          <Divider />
-          <CardBody>
-            <Skeleton isLoaded={!pengumumanQuery.isLoading} rounded="lg">
-              <Swiper
-                modules={[Navigation, Pagination, A11y]}
-                spaceBetween={50}
-                slidesPerView={1}
-                navigation
-                pagination={{ clickable: true }}
-                loop
+      <Skeleton isLoaded={!pengumumanQuery.isLoading} rounded="lg">
+        <Swiper
+          modules={[Navigation, Pagination, A11y]}
+          spaceBetween={50}
+          slidesPerView={1}
+          navigation
+          pagination={{ clickable: true }}
+          loop
+        >
+          {pengumuman?.map((item) => (
+            <SwiperSlide className="items-center" key={item.id}>
+              <Box
+                w={"full"}
+                h={[300, 500]}
+                display={"flex"}
+                alignItems={"center"}
+                justifyContent={"center"}
               >
-                {pengumuman?.map((item) => (
-                  <SwiperSlide className="items-center" key={item.id}>
-                    <Box
-                      w={"full"}
-                      h={[200, 500]}
-                      display={"flex"}
-                      alignItems={"center"}
-                      justifyContent={"center"}
-                    >
-                      <Image
-                        alt={item.id}
-                        src={`${process.env.API_URL}/assets/${item.image}?access_token=${process.env.API_TOKEN}&quality=20`}
-                        fill
-                        style={{ objectFit: "cover" }}
-                      />
-                    </Box>
-                  </SwiperSlide>
-                ))}
-              </Swiper>
-            </Skeleton>
-          </CardBody>
-        </Card>
-      </Box>
+                <Image
+                  alt={item.id}
+                  src={`${process.env.API_URL}/assets/${item.image}?access_token=${process.env.API_TOKEN}&quality=20`}
+                  fill
+                  style={{ objectFit: "cover" }}
+                />
+              </Box>
+            </SwiperSlide>
+          ))}
+        </Swiper>
+      </Skeleton>
     </Stack>
   );
 }
