051596b263c635ebef0929f97056d79b916015f5
abduromanov <abduromanov@users.noreply.github.com>
2023-04-01 17:45:28 +0000

    Prettified Code!

--- src/pages/admin/pinjaman/murobahah/[id]/index.tsx
+++ src/pages/admin/pinjaman/murobahah/[id]/index.tsx
@@ -148,27 +148,41 @@ export default function PageDetailMurobahah() {
   const rangkumanPembayaranTotal = {
     sisaCicilan: {
       cicilan: useMemo(
-        () => detailMurobahah?.lunas ? 0 :
-          (detailMurobahah?.totalPinjaman || 0) -
-          rincianPembayaranTotal.cicilan,
+        () =>
+          detailMurobahah?.lunas
+            ? 0
+            : (detailMurobahah?.totalPinjaman || 0) -
+              rincianPembayaranTotal.cicilan,
         [detailMurobahah, rincianPembayaranTotal.cicilan]
       ),
       margin: useMemo(
-        () => detailMurobahah?.lunas ? 0 :
-          (detailMurobahah?.totalMargin || 0) - rincianPembayaranTotal.margin,
+        () =>
+          detailMurobahah?.lunas
+            ? 0
+            : (detailMurobahah?.totalMargin || 0) -
+              rincianPembayaranTotal.margin,
         [detailMurobahah, rincianPembayaranTotal.margin]
       ),
       total: useMemo(
-        () => detailMurobahah?.lunas ? 0 : (detailMurobahah?.total || 0) - rincianPembayaranTotal.total,
+        () =>
+          detailMurobahah?.lunas
+            ? 0
+            : (detailMurobahah?.total || 0) - rincianPembayaranTotal.total,
         [detailMurobahah, rincianPembayaranTotal.total]
       ),
     },
     totalTenor: useMemo(
-      () => detailMurobahah?.lunas ? 0 : (detailMurobahah?.tenor || 0) - rincianPembayaranTotal.tenorBayar,
+      () =>
+        detailMurobahah?.lunas
+          ? 0
+          : (detailMurobahah?.tenor || 0) - rincianPembayaranTotal.tenorBayar,
       [detailMurobahah, rincianPembayaranTotal.tenorBayar]
     ),
     bulanTidakSesuai: useMemo(
-      () => detailMurobahah?.lunas ? 0 : _.sumBy(mutasiMurobahah, "sum.bulanTidakSesuai"),
+      () =>
+        detailMurobahah?.lunas
+          ? 0
+          : _.sumBy(mutasiMurobahah, "sum.bulanTidakSesuai"),
       [detailMurobahah, mutasiMurobahah]
     ),
   };
@@ -257,74 +271,87 @@ export default function PageDetailMurobahah() {
         </Flex>
       </Flex>
 
-      <Stack
-        direction={["column", "row"]}
-        spacing={5}
-      >
-        <Card variant='outline' shadow='sm' w={['full', '50%']} p={5}>
+      <Stack direction={["column", "row"]} spacing={5}>
+        <Card variant="outline" shadow="sm" w={["full", "50%"]} p={5}>
           <VStack spacing={3} divider={<Divider />}>
-            <Heading mb={3} size="md">Rincian Cicilan</Heading>
+            <Heading mb={3} size="md">
+              Rincian Cicilan
+            </Heading>
 
-            <VStack spacing={3} alignItems="start" w='full'>
-              <HStack flexWrap="wrap" w='full' justifyContent='space-between'>
-                <Text fontWeight="bold">
-                  Nama
-                </Text>
-                <Skeleton isLoaded={!detailMurobahahQuery.isLoading} minW='40%' textAlign='right'>
+            <VStack spacing={3} alignItems="start" w="full">
+              <HStack flexWrap="wrap" w="full" justifyContent="space-between">
+                <Text fontWeight="bold">Nama</Text>
+                <Skeleton
+                  isLoaded={!detailMurobahahQuery.isLoading}
+                  minW="40%"
+                  textAlign="right"
+                >
                   <Text>{detailMurobahah?.anggota?.nama}</Text>
                 </Skeleton>
               </HStack>
-              <HStack flexWrap="wrap" w='full' justifyContent='space-between'>
-                <Text fontWeight="bold">
-                  Pembiayaan
-                </Text>
-                <Skeleton isLoaded={!detailMurobahahQuery.isLoading} minW='40%' textAlign='right'>
+              <HStack flexWrap="wrap" w="full" justifyContent="space-between">
+                <Text fontWeight="bold">Pembiayaan</Text>
+                <Skeleton
+                  isLoaded={!detailMurobahahQuery.isLoading}
+                  minW="40%"
+                  textAlign="right"
+                >
                   <Text>{detailMurobahah?.pembiayaan}</Text>
                 </Skeleton>
               </HStack>
             </VStack>
 
-            <VStack spacing={3} alignItems="start" w='full'>
-              <HStack flexWrap="wrap" w='full' justifyContent='space-between'>
-                <Text fontWeight="bold">
-                  Pinjaman
-                </Text>
-                <Skeleton isLoaded={!detailMurobahahQuery.isLoading} minW='40%' textAlign='right'>
+            <VStack spacing={3} alignItems="start" w="full">
+              <HStack flexWrap="wrap" w="full" justifyContent="space-between">
+                <Text fontWeight="bold">Pinjaman</Text>
+                <Skeleton
+                  isLoaded={!detailMurobahahQuery.isLoading}
+                  minW="40%"
+                  textAlign="right"
+                >
                   <Text>{toIDR(detailMurobahah?.totalPinjaman)}</Text>
                 </Skeleton>
               </HStack>
-              <HStack flexWrap="wrap" w='full' justifyContent='space-between'>
-                <Text fontWeight="bold">
-                  Margin
-                </Text>
-                <Skeleton isLoaded={!detailMurobahahQuery.isLoading} minW='40%' textAlign='right'>
+              <HStack flexWrap="wrap" w="full" justifyContent="space-between">
+                <Text fontWeight="bold">Margin</Text>
+                <Skeleton
+                  isLoaded={!detailMurobahahQuery.isLoading}
+                  minW="40%"
+                  textAlign="right"
+                >
                   <Text>{toIDR(detailMurobahah?.totalMargin)}</Text>
                 </Skeleton>
               </HStack>
-              <HStack flexWrap="wrap" w='full' justifyContent='space-between'>
-                <Text fontWeight="bold">
-                  DP
-                </Text>
-                <Skeleton isLoaded={!detailMurobahahQuery.isLoading} minW='40%' textAlign='right'>
+              <HStack flexWrap="wrap" w="full" justifyContent="space-between">
+                <Text fontWeight="bold">DP</Text>
+                <Skeleton
+                  isLoaded={!detailMurobahahQuery.isLoading}
+                  minW="40%"
+                  textAlign="right"
+                >
                   <Text>{toIDR(detailMurobahah?.dp)}</Text>
                 </Skeleton>
               </HStack>
             </VStack>
 
-            <VStack spacing={3} alignItems="start" w='full' color='brand.500'>
-              <HStack flexWrap="wrap" w='full' justifyContent='space-between'>
-                <Text fontWeight="bold">
-                  Total
-                </Text>
-                <Skeleton isLoaded={!detailMurobahahQuery.isLoading} minW='40%' textAlign='right'>
+            <VStack spacing={3} alignItems="start" w="full" color="brand.500">
+              <HStack flexWrap="wrap" w="full" justifyContent="space-between">
+                <Text fontWeight="bold">Total</Text>
+                <Skeleton
+                  isLoaded={!detailMurobahahQuery.isLoading}
+                  minW="40%"
+                  textAlign="right"
+                >
                   <Text>{toIDR(detailMurobahah?.total)}</Text>
                 </Skeleton>
               </HStack>
-              <HStack flexWrap="wrap" w='full' justifyContent='space-between'>
-                <Text fontWeight="bold">
-                  Tenor
-                </Text>
-                <Skeleton isLoaded={!detailMurobahahQuery.isLoading} minW='40%' textAlign='right'>
+              <HStack flexWrap="wrap" w="full" justifyContent="space-between">
+                <Text fontWeight="bold">Tenor</Text>
+                <Skeleton
+                  isLoaded={!detailMurobahahQuery.isLoading}
+                  minW="40%"
+                  textAlign="right"
+                >
                   <Text>{detailMurobahah?.tenor}</Text>
                 </Skeleton>
               </HStack>
@@ -332,46 +359,73 @@ export default function PageDetailMurobahah() {
           </VStack>
         </Card>
 
-        <Card variant='outline' shadow='sm' w={['full', '50%']} p={5}>
-          <VStack justify='space-between' alignItems='stretch' w='full' h='full' spacing={3}>
+        <Card variant="outline" shadow="sm" w={["full", "50%"]} p={5}>
+          <VStack
+            justify="space-between"
+            alignItems="stretch"
+            w="full"
+            h="full"
+            spacing={3}
+          >
             <VStack spacing={3} divider={<Divider />}>
-              <Heading mb={3} size="md">Cicilan Perbulan</Heading>
+              <Heading mb={3} size="md">
+                Cicilan Perbulan
+              </Heading>
 
-              <VStack spacing={3} alignItems="start" w='full'>
-                <HStack flexWrap="wrap" w='full' justifyContent='space-between'>
-                  <Text fontWeight="bold">
-                    Pinjaman/bulan
-                  </Text>
-                  <Skeleton isLoaded={!detailMurobahahQuery.isLoading} minW='40%' textAlign='right'>
+              <VStack spacing={3} alignItems="start" w="full">
+                <HStack flexWrap="wrap" w="full" justifyContent="space-between">
+                  <Text fontWeight="bold">Pinjaman/bulan</Text>
+                  <Skeleton
+                    isLoaded={!detailMurobahahQuery.isLoading}
+                    minW="40%"
+                    textAlign="right"
+                  >
                     <Text>{toIDR(detailMurobahah?.pinjaman)}</Text>
                   </Skeleton>
                 </HStack>
-                <HStack flexWrap="wrap" w='full' justifyContent='space-between'>
-                  <Text fontWeight="bold">
-                    Margin/bulan
-                  </Text>
-                  <Skeleton isLoaded={!detailMurobahahQuery.isLoading} minW='40%' textAlign='right'>
+                <HStack flexWrap="wrap" w="full" justifyContent="space-between">
+                  <Text fontWeight="bold">Margin/bulan</Text>
+                  <Skeleton
+                    isLoaded={!detailMurobahahQuery.isLoading}
+                    minW="40%"
+                    textAlign="right"
+                  >
                     <Text>{toIDR(detailMurobahah?.margin)}</Text>
                   </Skeleton>
                 </HStack>
               </VStack>
             </VStack>
 
-            <VStack spacing={3} alignItems="start" w='full'>
+            <VStack spacing={3} alignItems="start" w="full">
               <Divider />
-              <HStack flexWrap="wrap" w='full' justifyContent='space-between'>
-                <Text fontWeight="bold">
-                  Total
-                </Text>
-                <Skeleton isLoaded={!detailMurobahahQuery.isLoading} minW='40%' textAlign='right'>
+              <HStack flexWrap="wrap" w="full" justifyContent="space-between">
+                <Text fontWeight="bold">Total</Text>
+                <Skeleton
+                  isLoaded={!detailMurobahahQuery.isLoading}
+                  minW="40%"
+                  textAlign="right"
+                >
                   <Text>{toIDR(~~(detailMurobahah?.cicilan || 0))}</Text>
                 </Skeleton>
               </HStack>
-              <Flex justify='end' w='full'>
-                <Skeleton isLoaded={!detailMurobahahQuery.isLoading} minW='40%' textAlign='right'>
-                  <Tag colorScheme={detailMurobahah?.lunas ? "green" : "yellow"} rounded="md" variant="solid">
-                    <TagLeftIcon boxSize="12px" as={detailMurobahah?.lunas ? CheckIcon : XMarkIcon} />
-                    <TagLabel>{detailMurobahah?.lunas ? "Lunas" : "Belum Lunas"}</TagLabel>
+              <Flex justify="end" w="full">
+                <Skeleton
+                  isLoaded={!detailMurobahahQuery.isLoading}
+                  minW="40%"
+                  textAlign="right"
+                >
+                  <Tag
+                    colorScheme={detailMurobahah?.lunas ? "green" : "yellow"}
+                    rounded="md"
+                    variant="solid"
+                  >
+                    <TagLeftIcon
+                      boxSize="12px"
+                      as={detailMurobahah?.lunas ? CheckIcon : XMarkIcon}
+                    />
+                    <TagLabel>
+                      {detailMurobahah?.lunas ? "Lunas" : "Belum Lunas"}
+                    </TagLabel>
                   </Tag>
                 </Skeleton>
               </Flex>
@@ -380,11 +434,9 @@ export default function PageDetailMurobahah() {
         </Card>
       </Stack>
 
-      <Card variant='outline' m={5}>
+      <Card variant="outline" m={5}>
         <CardHeader>
-          <Heading size="md">
-            Rincian Pembayaran
-          </Heading>
+          <Heading size="md">Rincian Pembayaran</Heading>
         </CardHeader>
         <Divider />
         {mutasiMurobahahQuery.isLoading && (
@@ -392,7 +444,7 @@ export default function PageDetailMurobahah() {
         )}
         <CardBody>
           <VStack alignItems="start" w={200} mb={5}>
-            <Text fontSize='sm'>Tahun</Text>
+            <Text fontSize="sm">Tahun</Text>
             <Select
               onChange={(e) => setSelectedTahun(e.target.value)}
               placeholder="Semua"
@@ -464,8 +516,8 @@ export default function PageDetailMurobahah() {
                 />
               </Tbody>
               <Tfoot>
-                <Tr fontWeight='semibold'>
-                  <Td textTransform='uppercase'>Total</Td>
+                <Tr fontWeight="semibold">
+                  <Td textTransform="uppercase">Total</Td>
                   <Td></Td>
                   <Td>{rincianPembayaranTotal.tenorBayar}</Td>
                   <Td>{rincianPembayaranTotal.bulanTidakSesuai}</Td>
@@ -482,9 +534,7 @@ export default function PageDetailMurobahah() {
 
       <Card boxShadow="md" mx={5} mb={5}>
         <CardHeader>
-          <Heading size="md">
-            Rangkuman Pembayaran
-          </Heading>
+          <Heading size="md">Rangkuman Pembayaran</Heading>
         </CardHeader>
         <Divider />
         <CardBody>
@@ -499,7 +549,10 @@ export default function PageDetailMurobahah() {
                 </Tr>
               </Thead>
               <Tbody>
-                <TableRangkumanPembayaran item={rangkumanPembayaranTotal} item2={rincianPembayaranTotal} />
+                <TableRangkumanPembayaran
+                  item={rangkumanPembayaranTotal}
+                  item2={rincianPembayaranTotal}
+                />
               </Tbody>
             </Table>
           </TableContainer>
